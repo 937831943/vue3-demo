2e38ffe8defc71fdabef20dd7465181467d21805
金宇轩 <jinyuxuan@jinyuxuandeMacBook-Air.local>
2023-01-05 20:33:25 +0800

    vue3初次尝试

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div>{{name}}</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+    setup() {
+        let name = ref('wocao')
+        console.log(name);
+        return {
+            name
+        }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
